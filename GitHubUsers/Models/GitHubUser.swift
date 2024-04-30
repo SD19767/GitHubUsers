5622c6b99de01367474471c9458c998e1834024a
Alvin <alvin.tseng@icloud.com>
2024-04-30 16:29:36 +0800

    Update GitHubUser to codable.

--- GitHubUsers/Models/GitHubUser.swift
+++ GitHubUsers/Models/GitHubUser.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct GitHubUser: Decodable {
+struct GitHubUser: Codable {
     let login: String
     let id: Int
     let nodeID: String
@@ -18,8 +18,15 @@ struct GitHubUser: Decodable {
     let subscriptionsURL, organizationsURL, reposURL: String
     let eventsURL: String
     let receivedEventsURL: String
-    let type: TypeEnum
+    let type: String
     let siteAdmin: Bool
+    let name, company: String?
+    let blog: String?
+    let location, email: String?
+    let hireable: Bool?
+    let bio, twitterUsername: String?
+    let publicRepos, publicGists, followers, following: Int?
+    let createdAt, updatedAt: String?
 
     enum CodingKeys: String, CodingKey {
         case login, id
@@ -39,10 +46,12 @@ struct GitHubUser: Decodable {
         case receivedEventsURL = "received_events_url"
         case type
         case siteAdmin = "site_admin"
+        case name, company, blog, location, email, hireable, bio
+        case twitterUsername = "twitter_username"
+        case publicRepos = "public_repos"
+        case publicGists = "public_gists"
+        case followers, following
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
     }
-}
-
-enum TypeEnum: String, Codable {
-    case organization = "Organization"
-    case user = "User"
 }
